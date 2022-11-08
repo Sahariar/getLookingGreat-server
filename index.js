b188f1933d0dc854fcb1ac47414fc3ae4b3bd187
Sahariar Kabir <sronok@gmail.com>
2022-11-08 11:10:16 +0600

    added service db and information

--- index.js
+++ index.js
@@ -1,23 +1,48 @@
-const express = require('express');
-const cors = require('cors');
-const { MongoClient, ServerApiVersion } = require('mongodb');
-require('dotenv').config()
+const express = require("express");
+const cors = require("cors");
+const { MongoClient, ServerApiVersion } = require("mongodb");
+require("dotenv").config();
 const app = express();
-const port = process.env.PORT || 4000
- 
+const port = process.env.PORT || 4000;
+
 // middleware
 app.use(cors());
 app.use(express.json());
- 
+
+// mongoDB
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@getlookinggreat.hhjymrn.mongodb.net/?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, {
+	useNewUrlParser: true,
+	useUnifiedTopology: true,
+	serverApi: ServerApiVersion.v1,
+});
+
 const run = async () => {
-}
-run().catch(console.dir); 
- 
- 
-app.get('/' , (req ,res) =>{
-res.send('server is running');
-})
- 
-app.listen(port, ()=>{
-console.log(`Server is running at ${port}`);
-})
+	try {
+		const servicesCollection = client.db("gLGDb").collection("services");
+        
+        app.get('/services' , async(req ,res) =>{
+
+            const query = {
+
+            }
+            const cursor = servicesCollection.find(query);
+            const result = await cursor.toArray();
+        res.send(result);
+        })
+        
+
+
+	} finally {
+	}
+};
+run().catch(console.dir);
+
+app.get("/", (req, res) => {
+	res.send("server is running");
+});
+
+app.listen(port, () => {
+	console.log(`Server is running at ${port}`);
+});
